6ddfd115362dec45cf100e316e6068a19f3e36b3
doniurek <aldonaglebicka@gmail.com>
2023-02-16 16:46:57 +0100

    Import invoices list, add table and CSS

--- index.js
+++ index.js
@@ -5,5 +5,17 @@ async function getInvoices(input) {
     }
   });
 
-  console.log(await response.json())
+  let result = await response.json()
+
+  const tblBody = document.getElementById("tableBody")
+
+  for (let i = 0; i < result.data.length; i++) {
+    const row = document.createElement("tr");
+    row.insertCell(0).innerHTML = result.data[i].number
+    row.insertCell(1).innerHTML = result.data[i].total / 100
+    row.insertCell(2).innerHTML = result.data[i].currency
+    row.insertCell(3).innerHTML = new Date(result.data[i].created * 1000).toLocaleDateString()
+
+    tblBody.appendChild(row);
+  }
 }
