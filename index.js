29ce38167ad6c2d2193c15cb3b7c7f9b63e20a6e
doniurek <aldonaglebicka@gmail.com>
2023-02-22 21:04:37 +0100

    Move to jQuery

--- index.js
+++ index.js
@@ -1,47 +1,54 @@
+$(document).ready(function() {
+  $('#submitbtn').click(function() {
+    sessionStorage.getItem('apiKey')
+      ? getInvoices(sessionStorage.getItem('apiKey'))
+      : alert('Please input and save your API key first!')
+  })
+});
+
 async function getInvoices(input) {
-
-  let response = await fetch('https://api.stripe.com/v1/invoices?status=open', {
+  $.ajax({
+    type: 'GET',
+    url: 'https://api.stripe.com/v1/invoices?status=open',
+    dataType: 'json',
     headers: {
-      Authorization : `Bearer ${input}`
+      'Authorization' : `Bearer ${input}`
     }
-  });
+  })
+    .done(function(json) {
+      let tblBody = $('table tbody')
 
-  let result = await response.json()
-
-  const tblBody = document.getElementById("tableBody")
-
-  for (let i = 0; i < result.data.length; i++) {
-    const row = document.createElement("tr");
-    row.insertCell(0).innerHTML = result.data[i].number
-    row.insertCell(1).innerHTML = result.data[i].id
-    row.insertCell(2).innerHTML = result.data[i].total / 100
-    row.insertCell(3).innerHTML = result.data[i].currency
-    row.insertCell(4).innerHTML = new Date(result.data[i].created * 1000).toLocaleDateString()
-    row.insertCell(5).innerHTML = '<input type="button" id="paybtn" value="Mark as paid" onClick="payInvoice(this)">'
-
-    tblBody.appendChild(row);
+      for (let i = 0; i < json.data.length; i++) {
+        content += '<tr>'
+        content += '<td>' + json.data[i].number + '</td>'
+        content += '<td>' + json.data[i].id + '</td>'
+        content += '<td>' + json.data[i].total / 100 + '</td>'
+        content += '<td>' + json.data[i].currency + '</td>'
+        content += '<td>' + new Date(json.data[i].created * 1000).toLocaleDateString() + '</td>'
+        content += '<td>' + '<input type="button" id="paybtn" value="Mark as paid" onClick="payInvoice(this)">' + '</td>'
+        content += '</tr>'
+      }
+      tblBody.append(content)
+    })
   }
-}
 
 function saveApiKey(input) {
-  sessionStorage.setItem("apiKey", input)
+  sessionStorage.setItem('apiKey', input)
 }
 
 async function payInvoice(obj) {
   if (confirm('Are you sure you want to pay this invoice?')) {
-    let index = obj.parentNode.parentNode.rowIndex
-    let table = document.getElementById("invoicesTable");
-    let invoiceID = table.rows[index].cells[1].innerHTML
+    let index = $(obj).parent().parent().index()
+    let invoiceID = $('#invoicesTable').find('tbody tr').eq(index).find('td').eq(1).html()
 
-    console.log(invoiceID)
-
-    let response = await fetch(`https://api.stripe.com/v1/invoices/${invoiceID}/pay?paid_out_of_band=true`, {
-      method: 'POST',
+    $.ajax({
+      type: 'POST',
+      url: `https://api.stripe.com/v1/invoices/${invoiceID}/pay?paid_out_of_band=true`,
       headers: {
-        Authorization : `Bearer ${sessionStorage.getItem("apiKey")}`
+        'Authorization' : `Bearer ${sessionStorage.getItem("apiKey")}`
       }
-    });
+    })
 
-    table.deleteRow(index)
+    $('tbody tr').eq(index).remove()
   }
 }
